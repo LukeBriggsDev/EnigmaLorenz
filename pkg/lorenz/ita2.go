8c4229c96953d81342314eef73347d9d859713a9
Luke Briggs <lukebriggs02@gmail.com>
2022-07-26 14:27:51 +0100

    Implement conversion from ASCII to ITA2

---
+++ pkg/lorenz/ita2.go
@@ -0,0 +1,141 @@
+package lorenz
+
+import "errors"
+
+type bimap struct {
+	forwardMap map[byte]byte
+	reverseMap map[byte]byte
+}
+
+func bimapFromSlice(s []byte) bimap {
+	m := bimap{}
+	for idx, val := range s {
+		m.Add(byte(idx), val)
+	}
+	return m
+}
+
+func (m *bimap) Add(k byte, v byte) {
+	m.forwardMap[k] = v
+	m.reverseMap[v] = k
+}
+
+func (m *bimap) GetITA2Code(k byte) (byte, bool) {
+	v, Ok := m.reverseMap[k]
+	return v, Ok
+}
+
+func (m *bimap) GetASCII(k byte) (byte, bool) {
+	v, Ok := m.forwardMap[k]
+	return v, Ok
+}
+
+type ITA2 struct {
+	letterAlphabet bimap
+	figureAlphabet bimap
+}
+
+func newITA2() ITA2 {
+	letter := []byte{
+		'\x00', // NULL
+		'E',
+		'\x0A', // LF
+		'A',
+		' ',
+		'S',
+		'I',
+		'U',
+		'\x0D', // CR
+		'D',
+		'R',
+		'J',
+		'N',
+		'F',
+		'C',
+		'K',
+		'T',
+		'Z',
+		'L',
+		'W',
+		'H',
+		'Y',
+		'P',
+		'Q',
+		'O',
+		'B',
+		'G',
+		'\x0F', // Shift In
+		'M',
+		'X',
+		'V',
+		'\x0E', // Shift Out
+	}
+
+	figure := []byte{
+		'\x00', // NULL
+		'3',
+		'\x0A', // LF
+		'-',
+		' ',
+		'\'',
+		'8',
+		'7',
+		'\x0D', // CR
+		'\x05', // Enquiry
+		'4',
+		'\x07', // Bell
+		',',
+		'!',
+		':',
+		'(',
+		'5',
+		'+',
+		')',
+		'2',
+		'$',
+		'6',
+		'0',
+		'1',
+		'9',
+		'?',
+		'&',
+		'\x0F', // Shift In
+		'.',
+		'/',
+		';',
+		'\x0E', // Shift Out
+	}
+
+	ITA := ITA2{
+		letterAlphabet: bimapFromSlice(letter),
+		figureAlphabet: bimapFromSlice(figure),
+	}
+
+	return ITA
+}
+
+func (alphabet *ITA2) AsciiToITA2(s string) ([]byte, error) {
+	encoded := []byte{}
+	inLetterShift := true
+	for _, char := range s {
+		letter, letterExist := alphabet.letterAlphabet.GetITA2Code(byte(char))
+		if !letterExist {
+			figure, figureExists := alphabet.figureAlphabet.GetITA2Code(byte(char))
+			if !figureExists {
+				return []byte(""), errors.New("invalid characters in input string")
+			}
+			if inLetterShift {
+				encoded = append(encoded, 0x0E)
+				inLetterShift = !inLetterShift
+			}
+			encoded = append(encoded, figure)
+		} else {
+			if !inLetterShift {
+				encoded = append(encoded, 0x0F)
+				inLetterShift = !inLetterShift
+			}
+			encoded = append(encoded, letter)
+		}
+	}
+	return encoded, nil
+}
